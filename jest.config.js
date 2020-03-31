878d99c9dd6951747139c44503f3ad6932202c13
Max Schmitt <max@schmitt.mx>
2020-04-01 01:15:05 +0200

    feat: using jest-playwright (#2)

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,10 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  preset: 'jest-playwright-preset',
   testMatch: ['**/src/**/*.test.ts'],
   collectCoverage: true,
-  collectCoverageFrom: ["**/src/**/*.ts"]
+  collectCoverageFrom: ["**/src/**/*.ts"],
+  transform: {
+    "^.+\\.ts$": "ts-jest"
+  },
+  setupFilesAfterEnv: ["<rootDir>/lib/index.js"]
 };
