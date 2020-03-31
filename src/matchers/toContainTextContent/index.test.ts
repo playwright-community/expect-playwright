26eaad75f543d5ef8ad24e1364cec3ebb41cb975
Max Schmitt <max@schmitt.mx>
2020-04-01 01:13:17 +0200

    feat: using jest-playwright

--- src/matchers/toContainTextContent/index.test.ts
+++ src/matchers/toContainTextContent/index.test.ts
@@ -1,24 +1,6 @@
-import "../../../"
-
-import '../index'
-
-import playwright, { Page, Browser } from 'playwright-chromium'
+/// <reference types="jest-playwright-preset" />
 
 describe("toContainTextContent", () => {
-  let page: Page
-  let browser: Browser;
-  beforeAll(async () => {
-    browser = await playwright.chromium.launch()
-  })
-  afterAll(async () => {
-    await browser.close()
-  })
-  afterEach(async () => {
-    await page.close()
-  })
-  beforeEach(async () => {
-    page = await browser.newPage()
-  })
   it("should be able to test positive for a selector", async () => {
     await page.evaluate(() => {
       document.write(`<div id="foobar">Bar</div>`)
