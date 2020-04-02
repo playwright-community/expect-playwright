6ac4fa296d71181ad54b4090f089ed7c740efcbd
Max Schmitt <max@schmitt.mx>
2020-04-02 20:03:17 +0200

    fix: toHaveText negative case

--- src/matchers/toHaveText/index.ts
+++ src/matchers/toHaveText/index.ts
@@ -2,18 +2,25 @@ import { SyncExpectationResult } from 'expect/build/types'
 import { getElementText, quote, InputArguments } from '../utils'
 
 const toHaveText = async (...args: InputArguments): Promise<SyncExpectationResult> => {
-  const { elementHandle, selector, expectedValue } = await getElementText(...args)
-  /* istanbul ignore next */
-  const actualTextContent = await elementHandle.evaluate((el) => el.textContent)
-  if (actualTextContent?.includes(expectedValue)) {
-    return {
-      pass: true,
-      message: () => `${quote(expectedValue)} is included in ${quote(actualTextContent)}.`
+  try {
+    const { elementHandle, selector, expectedValue } = await getElementText(...args)
+    /* istanbul ignore next */
+    const actualTextContent = await elementHandle.evaluate((el) => el.textContent)
+    if (actualTextContent?.includes(expectedValue)) {
+      return {
+        pass: true,
+        message: () => `${quote(expectedValue)} is included in ${quote(actualTextContent)}.`
+      }
     }
-  }
-  return {
-    pass: false,
-    message: () => `${quote(expectedValue)} is not included in ${quote(actualTextContent)}${selector ? ' of ' + quote(selector) + "." : '.'}`
+    return {
+      pass: false,
+      message: () => `${quote(expectedValue)} is not included in ${quote(actualTextContent)}${selector ? ' of ' + quote(selector) + "." : '.'}`
+    }
+  } catch (err) {
+    return {
+      pass: false,
+      message: () => err.toString()
+    }
   }
 }
 
