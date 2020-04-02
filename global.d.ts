c2df1c0a4de3b50ec2603a7b5b48c61d4fe11c29
Max Schmitt <max@schmitt.mx>
2020-04-02 18:53:26 +0200

    feat: usage without Jest

--- global.d.ts
+++ global.d.ts
@@ -1,15 +1,16 @@
 import { PageWaitForSelectorOptions } from "playwright-core";
 
-export { };
+export interface PlaywrightMatchers<R> {
+  toHaveText(selector: string, value: string, options?: PageWaitForSelectorOptions): Promise<R>;
+  toHaveText(value: string, options?: PageWaitForSelectorOptions): Promise<R>;
+  toEqualText(selector: string, value: string, options?: PageWaitForSelectorOptions): Promise<R>;
+  toEqualText(value: string, options?: PageWaitForSelectorOptions): Promise<R>;
+  toHaveSelector(selector: string, options?: PageWaitForSelectorOptions): Promise<R>;
+}
 
 declare global {
   namespace jest {
-    interface Matchers<R> {
-      toHaveText(selector: string, value: string, options?: PageWaitForSelectorOptions): Promise<R>;
-      toHaveText(value: string, options?: PageWaitForSelectorOptions): Promise<R>;
-      toEqualText(selector: string, value: string, options?: PageWaitForSelectorOptions): Promise<R>;
-      toEqualText(value: string, options?: PageWaitForSelectorOptions): Promise<R>;
-      toHaveSelector(selector: string, options?: PageWaitForSelectorOptions): Promise<R>;
-    }
+    interface Matchers<R> extends PlaywrightMatchers<R> { }
   }
 }
+
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,24 @@
 import matchers from './matchers';
+import { Page, ElementHandle } from 'playwright-core';
+import type { PlaywrightMatchers } from '../global'
 
 // @ts-ignore
 const jestExpect = global.expect;
 
 jestExpect.extend(matchers);
+
+
+const expectWrapper = (pageOrElement: Page | ElementHandle): PlaywrightMatchers<boolean> =>
+  Object.entries(matchers).reduce((acc, [name, matcher]) => ({
+    ...acc,
+    [name]: async (...args: any[]) => {
+      // @ts-ignore
+      const result = await matcher(pageOrElement, ...args)
+      if (!result.pass) {
+        throw new Error(result.message())
+      }
+      return true
+    }
+  }), {} as PlaywrightMatchers<boolean>)
+
+export default expectWrapper
