152bfbf6a64af765033dfb1977df83419548829e
mmarkelov <maks-markel@mail.ru>
2021-01-14 23:27:50 +0300

    Fix getElementText util function

--- src/matchers/utils.ts
+++ src/matchers/utils.ts
@@ -31,13 +31,13 @@ export type InputArguments = [Page | ElementHandle, string?, (string | PageWaitF
 const lastElementHasType = (args: InputArguments, type: "string" | "object"): boolean => typeof args[args.length - 1] === type
 
 export const getElementText = async (...args: InputArguments): Promise<getElementTextReturn> => {
+  const type = detectExpectType(args[0])
   /**
   * Handle the following cases:
   * - expect(page).foo("bar")
   * - expect(element).foo("bar")
   */
   if (args.length === 2) {
-    const type = detectExpectType(args[0])
     if (type === ExpectTypeElementHandle) {
       return {
         elementHandle: args[0] as ElementHandle,
@@ -54,18 +54,34 @@ export const getElementText = async (...args: InputArguments): Promise<getElemen
    * Handle the following case:
    * - expect(page).foo("#foo", "bar")
    */
-  if (args.length === 3 && lastElementHasType(args, "string") || args.length === 4 && lastElementHasType(args, "object")) {
-    const selector = args[1] as string
+  if (type === ExpectTypePage) {
     const page = args[0] as Page
-    try {
-      await page.waitForSelector(selector, args[3] as PageWaitForSelectorOptions)
-    } catch (err) {
-      throw new Error(`Timeout exceed for element ${quote(selector)}`)
+    const selector = args[1] as string
+    if (args.length === 3) {
+      if (lastElementHasType(args, "string")) {
+        try {
+          await page.waitForSelector(selector)
+        } catch (err) {
+          throw new Error(`Timeout exceed for element ${quote(selector)}`)
+        }
+        return {
+          elementHandle: await page.$(selector) as ElementHandle,
+          expectedValue: args[2] as string,
+          selector
+        }
+      }
     }
-    return {
-      elementHandle: await page.$(selector) as ElementHandle,
-      expectedValue: args[2] as string,
-      selector
+    if (args.length === 4 && lastElementHasType(args, "object")) {
+      try {
+        await page.waitForSelector(selector, args[3] as PageWaitForSelectorOptions)
+      } catch (err) {
+        throw new Error(`Timeout exceed for element ${quote(selector)}`)
+      }
+      return {
+        elementHandle: await page.$(selector) as ElementHandle,
+        expectedValue: args[2] as string,
+        selector
+      }
     }
   }
   throw new Error(`Invalid input length: ${args.length}`)
