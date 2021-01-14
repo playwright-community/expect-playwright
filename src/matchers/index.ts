e35040ac6e988c8d429054f88819764cd3587203
Nick Stevens <sirnickylee@gmail.com>
2021-01-14 14:53:54 +0000

    Missing export from the new toHaveSelectorCount

--- src/matchers/index.ts
+++ src/matchers/index.ts
@@ -2,10 +2,12 @@ import toHaveText from './toHaveText'
 import toEqualText from './toEqualText'
 import toHaveSelector from './toHaveSelector'
 import toEqualValue from './toEqualValue'
+import toHaveSelectorCount from './toHaveSelectorCount'
 
 export default {
   toHaveText,
   toEqualText,
   toHaveSelector,
   toEqualValue,
+  toHaveSelectorCount
 }
