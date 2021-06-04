e1af75e54acc0e19fc04f37793f975a33833522e
Mark Skelton <mskelton@widen.com>
2021-06-04 14:58:11 -0500

    Update matcher and snap

--- src/matchers/toHaveSelector/index.ts
+++ src/matchers/toHaveSelector/index.ts
@@ -3,22 +3,35 @@ import { quote } from "../utils"
 import { Page } from "playwright-core"
 import { PageWaitForSelectorOptions } from "../../../global"
 
-const toHaveSelector = async (
+const toHaveSelector: jest.CustomMatcher = async function (
   page: Page,
   selector: string,
   options: PageWaitForSelectorOptions = {}
-): Promise<SyncExpectationResult> => {
-  try {
-    await page.waitForSelector(selector, options)
-    return {
-      pass: true,
-      message: () => `${quote(selector)} was found on the page.`,
-    }
-  } catch (err) {
-    return {
-      pass: false,
-      message: () => `${quote(selector)} could not be found on the page.`,
-    }
+): Promise<SyncExpectationResult> {
+  const pass = await page
+    .waitForSelector(selector, options)
+    .then(() => true)
+    .catch(() => false)
+
+  return {
+    pass: pass,
+    message: () => {
+      const not = this.isNot ? " not" : ""
+      const hint = this.utils.matcherHint(
+        "toHaveSelector",
+        undefined,
+        undefined,
+        { isNot: this.isNot, promise: this.promise }
+      )
+
+      return (
+        hint +
+        "\n\n" +
+        `Expected: page to${not} have selector ${this.utils.printExpected(
+          selector
+        )}`
+      )
+    },
   }
 }
 
