8aa83bb917b084522b9f008de5cb6b48c4b5ce75
Mark Skelton <mskelton@widen.com>
2021-06-04 09:10:03 -0500

    Finish toEqualUrl matcher

--- src/matchers/toEqualUrl/index.test.ts
+++ src/matchers/toEqualUrl/index.test.ts
@@ -6,7 +6,7 @@ expect.extend({ toEqualUrl })
 describe("toEqualUrl", () => {
   const urlPrefix = "http://i-do-not-exist.com"
 
-  it("positive", async () => {
+  beforeAll(async () => {
     await page.route("**/1.html", (route) => {
       route.fulfill({
         body: "123",
@@ -15,20 +15,47 @@ describe("toEqualUrl", () => {
         },
       })
     })
+  })
+
+  afterEach(async () => {
+    await page.setContent("")
+  })
+
+  it("positive in frame", async () => {
+    const myUrl = `${urlPrefix}/1.html`
+    await page.setContent(`<iframe src="${myUrl}"></iframe>`)
+    const iframe = await (await page.$("iframe"))!.contentFrame()
+    expect(iframe).toEqualUrl(myUrl)
+  })
+
+  it("positive", async () => {
     const myUrl = `${urlPrefix}/1.html`
     await page.goto(myUrl)
     expect(page).toEqualUrl(myUrl)
   })
+
   it("negative", async () => {
-    await page.route("**/1.html", (route) => {
-      route.fulfill({
-        body: "123",
-        headers: {
-          "Content-Type": "text/html",
-        },
-      })
-    })
     await page.goto(`${urlPrefix}/1.html`)
     await assertSnapshot(() => expect(page).toEqualUrl(`${urlPrefix}/2.html`))
+  })
+
+  describe("with 'not' usage", () => {
+    it("positive in frame", async () => {
+      const myUrl = `${urlPrefix}/1.html`
+      await page.setContent(`<iframe src="${myUrl}"></iframe>`)
+      const iframe = await (await page.$("iframe"))!.contentFrame()
+      expect(iframe).not.toEqualUrl("foobar")
+    })
+
+    it("positive", async () => {
+      await page.goto(`${urlPrefix}/1.html`)
+      expect(page).not.toEqualUrl("foobar")
+    })
+
+    it("negative", async () => {
+      const myUrl = `${urlPrefix}/1.html`
+      await page.goto(myUrl)
+      await assertSnapshot(() => expect(page).not.toEqualUrl(myUrl))
+    })
   })
 })
